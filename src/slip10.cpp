56f97f31ea59554043b0f055f99741eadeaafcd1
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-09-07 11:50:46 +0200

    align names of namespaces, targets ...

--- src/slip10.cpp
+++ src/slip10.cpp
@@ -1,7 +1,7 @@
-#include "crypto/qslip10.hpp"
+#include "esterv/crypto/qslip10.hpp"
 #include <QIODevice>
 #include <QMessageAuthenticationCode>
-namespace qcrypto
+namespace Esterv::Crypto
 {
 #define BIP32_HARDENED (1UL << 31)
 
